07efcddf0341831a13a2689d507a356d2c65ef95
iheteroclite <118612115+iheteroclite@users.noreply.github.com>
2022-12-19 10:23:52 +0000

    Test for card value working

--- blackjack/test/test_deck.py
+++ blackjack/test/test_deck.py
@@ -15,6 +15,16 @@ class DeckTestCase(unittest.TestCase):
         # check there's up to 8 decks in the deck :)
         self.assertTrue(number_of_cards in [52*n for n in range(8)])
 
+    def test_cards_integer_value(self):
+        card_int_val_truth = True
+        for card in self.deck.cards:
+            if not 1 <= card.value <= 11:
+                card_int_val_truth *= False
+            elif card.value % 1 != 0: #test for integer
+                card_int_val_truth *= False
+        self.assertTrue(card_int_val_truth)
+
+
 
 if __name__ == '__main__':
     unittest.main()
