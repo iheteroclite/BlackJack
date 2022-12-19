9f8c511dfae035bb7d8ad81e97a63ee28dd0b6b3
iheteroclite <118612115+iheteroclite@users.noreply.github.com>
2022-12-19 16:44:50 +0000

    dealer score displays last

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -11,6 +11,7 @@ def play():
     print('number of players selected was:', num_players)
 
     # make a deck
+    # min cards ensures there's at least 1 deck per 4 players
     min_decks = 2 if num_players > 4 else 1
     chose = player_choice(False, "decks", [n+min_decks for n in range(8)])
     num_decks = chose['choice']
@@ -21,6 +22,8 @@ def play():
     dealer_hand = Hand(deck, 'Dealer', 'dealer')
     player_hands = [Hand(deck, f'Player {x + 1}', 'player')
         for x in range(num_players)]
+    player_hands.append(dealer_hand)
+
 
     # players' turns
     for hand in player_hands:
@@ -43,7 +46,15 @@ def play():
                     continue
 
                 # get user input for if they want to hit, stand, or surrender
-                player_move = player_choice(hand)['choice']
+                player_move = ''
+                if hand.person == 'player':
+                    player_move = player_choice(hand)['choice']
+                elif hand.person == 'AI':
+                    # TODO: add an option to have AI make choices
+                    # can modify dealer draw so they only draw to specific val
+                    player_move = 'temp value'
+                elif hand.person == 'dealer':
+                    player_move = dealer_move(hand, 17)
 
                 print(player_move)
 
@@ -57,44 +68,42 @@ def play():
             else:
                 break
 
-        # TODO: check deck doesn't get down to 0 cards
         # OR make sure there are enough decks for the players
 
-        # Dealer's turn
-
-def hit(hand, deck):
-    # not yet used
-    print('hit me with your rhythm stick')
-    hand.hit(deck)
-
 def player_choice(hand, msg_str="", options = ['hit', 'stand', 'surrender']):
-    question = f"{hand.player}, What do you do?"
     questions = [
     inquirer.List('choice',
-        message = question if hand else f'Select number of {msg_str}:',
+        message = f"{hand.player}, What do you do?" if hand else f'Select number of {msg_str}:',
         choices = options,
         ),
     ]
     answers = inquirer.prompt(questions)
     return answers
 
-def check_twenty_one(hand, state='playing'):
-    if hand.get_total() == 21:
-        if hand.person == 'dealer':
-            #TODO: change all dealer aces into 1's are return false
-            # if hand.cards contains card with card.face =='ace'
-            #    return False
-            print('Dealer aces always remain 11 in this game variant')
+def check_twenty_one(hand, num = False, state='playing'):
+    total = hand.get_total()
+    if total == 21:
         if hand.state == 'draw':
             hand.state = 'blackjack'
         else:
             hand.state = 'twenty-one'
         return hand.state
-    elif hand.get_total() > 21:
+    elif total > 21:
+        if hand.person == 'dealer':
+            # for each ace, dealer takes its value as 1, and re-checks score
+            for card in hand.cards:
+                if card.face=='ace':
+                    card.value = 1
+                    if hand.get_total() <= 21: # re-check after changing ace
+                        return check_twenty_one(hand)
         hand.state = 'bust'
         return 'bust'
     return False
 
+def dealer_move(hand, num):
+    if hand.get_total() >= num:
+        return 'stand'
+    return 'hit'
 
 if __name__ == '__main__':
     play()
